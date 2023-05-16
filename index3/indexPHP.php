d39ef93761ebc8baa17504752ef2274f0fc5cfea
VoronWorks <anastasiya.voronova3@gmail.com>
2023-05-16 13:26:19 +0300

    Update indexPHP.php

--- index3/indexPHP.php
+++ index3/indexPHP.php
@@ -33,23 +33,20 @@ if ($errors) {
 
 $user = 'u52952'; 
 $pass = '1472414';
-$db = new PDO('mysql:host=localhost;port=8889;dbname=form', $user, $pass,
+$db = new PDO('mysql:host=localhost;port=8889;dbname=u52952', $user, $pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
 
 
 try {
-  $stmt = $db->prepare("INSERT INTO form (name,email,year,gender,limbs,bio) VALUES (:name,:email,:year,:gender,:limbs,:bio)");
-$stmt -> execute(['name'=>$_POST['field-name'], 'email'=>$_POST['field-email'],'year'=>$_POST['field-year'],
-'gender'=>$_POST['radio-group-1'],'limbs'=>$_POST['radio-group-2'],'bio'=>$_POST['field-name-2']]);
-  $stmt2 = $conn->prepare("SELECT id FROM form where name=$_POST['field-name']");
-  $stmt2->execute();
-  $row = $stmt2->fetch_assoc();
-  $id = $row['id'];
-    echo $id;
-foreach ($_POST['abilities'] as $ability) {
- $stmt = $db->prepare("INSERT INTO connect (s_id,pow_id) VALUES (:s_id,:pow_id)");
- $stmt -> execute(['s_id'=>$id,'pow_id'=>$ability]);
-  }
+  $stmt = $db->prepare("INSERT INTO form (name,email,year,gender,limbs,bio) VALUES 
+  (?,?,?,?,?,?)");
+  $stmt -> execute([$_POST['field-name'], $_POST['field-email'], $_POST['field-year'], $_POST['radio-group-1'],
+   $_POST['radio-group-2'], $_POST['field-name-2']]);
+  $id = $db->lastInsertId();
+  $stmt = $db->prepare("INSERT INTO connect (s_id, pow_id) VALUES (?,?)");
+    foreach ($_POST['abilities'] as $ability) {
+          $stmt->execute([$id, $ability]);
+        }
 }
 catch(PDOException $e){
   print('Error : ' . $e->getMessage());
