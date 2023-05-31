ef60c60ffb67d1207fcfd6b8e3e463eecdf803b0
VoronWorks <anastasiya.voronova3@gmail.com>
2023-05-31 13:55:45 +0300

    Update index4.php

--- index4/index4.php
+++ index4/index4.php
@@ -13,7 +13,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $errors = array();
   $errors['field-name'] = !empty($_COOKIE['field-name_error']);
   $errors['field-email'] = !empty($_COOKIE['field-email_error']);
-
+ $errors['gender'] = !empty($_COOKIE['gender_error']);
+  $errors['limbs'] = !empty($_COOKIE['limbs_error']);
+  $errors['abilities'] = !empty($_COOKIE['abilities_error']);
 
   if ($errors['field-name']) {
 
@@ -34,6 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   $values['field-name'] = empty($_COOKIE['field-name_value']) ? '' : $_COOKIE['field-name_value'];
   $values['field-email'] = empty($_COOKIE['field-email_value']) ? '' : $_COOKIE['field-email_value'];
   $values['field-year'] = empty($_COOKIE['field-year_value']) ? '' : $_COOKIE['field-year_value'];
+
   $values['field-name-2'] = empty($_COOKIE['field-name-2_value']) ? '' : $_COOKIE['field-name-2_value'];
   include('form4.php');
   exit();
@@ -42,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 else {
 
   $errors = FALSE;
-  if (empty($_POST['field-name']) || is_numeric($_POST['field-year'])) {
+  if (empty($_POST['field-name']) || is_numeric($_POST['field-name'])||!preg_match('/^([а-яА-ЯЁёa-zA-Z0-9_,.\s-]+)$/u', $_POST['field-name'])) {
     setcookie('field-name_error', '1', time() + 24 * 60 * 60);
     $errors = TRUE;
   }
@@ -71,7 +74,17 @@ else {
     $radio2=' checked ';
     setcookie('radio1', false, 600, '/');
   }
-
+if(isset($_POST['radio-group-2']) && $_POST['radio-group-1']=='1') {
+    setcookie('radio1', true, 600, '/');
+    $radio1=' checked ';
+    $radio2='';
+    setcookie('radio2', false, 600, '/');
+  } else if(isset($_POST['radio'])&& $_POST['radio']=='2') {
+    setcookie('radio2', true, 600, '/');
+    $radio1='';
+    $radio2=' checked ';
+    setcookie('radio1', false, 600, '/');
+  }
   if (empty($_POST['field-name-2'])) {
     setcookie('field-name-2_value'," ", time() + 30 * 24 * 60 * 60);
   }
@@ -90,6 +103,9 @@ else {
 
     setcookie('field-name_error', '', 100000);
     setcookie('field-email_error', '', 100000);
+    setcookie('gender_error', '', 100000);
+    setcookie('limbs_error', '', 100000);
+  
   }
 }
 
